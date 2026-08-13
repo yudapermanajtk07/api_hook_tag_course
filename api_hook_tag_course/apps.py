948633bd7a0e8364c9a6f12c4c7d1eb4ac0f050a
Yuda Permana <yuda.permana@ptbsp.com>
2026-08-13 18:16:46 +0700

    Fix plugin_app config to avoid missing urls.py error

--- api_hook_tag_course/apps.py
+++ api_hook_tag_course/apps.py
@@ -4,16 +4,7 @@ class ApiHookConfig(AppConfig):
     name = 'api_hook_tag_course'
     verbose_name = 'API Hook for Course Tags'
 
-    plugin_app = {
-        'url_config': {
-            'lms.djangoapp': {},
-            'cms.djangoapp': {},
-        },
-        'settings_config': {
-            'lms.djangoapp': {},
-            'cms.djangoapp': {},
-        },
-    }
+    plugin_app = {}
 
     def ready(self):
         # Mengimpor signals ketika Django sudah siap agar receiver terdaftar
